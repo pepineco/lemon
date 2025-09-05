fdbf6b10740f1c13741be6b3aa6bd921df90d1cf
Mayu Kihira <kihira@coocrew.com>
2025-09-05 23:03:19 +0900

    astro 3 -> 5

--- src/pages/api/likes.json.ts
+++ src/pages/api/likes.json.ts
@@ -1,54 +1,45 @@
-import type { APIRoute, APIContext } from 'astro'
+import type { APIRoute } from 'astro'
 import { getPostBySlug, incrementLikes } from '../../lib/notion/client'
 
-export const get: APIRoute = async ({ request }: APIContext) => {
+export const GET: APIRoute = async ({ request }) => {
   const url = new URL(request.url)
-  const params = new URLSearchParams(url.search)
-  const slug = params.get('slug')
+  const slug = url.searchParams.get('slug')
 
   if (!slug) {
     return new Response(null, { status: 400 })
   }
 
   const post = await getPostBySlug(slug)
-
   if (!post) {
     return new Response(null, { status: 404 })
   }
 
   return new Response(JSON.stringify({ likes: post.Like }), {
     status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
   })
 }
 
-export const post: APIRoute = async ({ request }: APIContext) => {
+export const POST: APIRoute = async ({ request }) => {
   const url = new URL(request.url)
-  const params = new URLSearchParams(url.search)
-  const slug = params.get('slug')
+  const slug = url.searchParams.get('slug')
 
   if (!slug) {
     return new Response(null, { status: 400 })
   }
 
   const post = await getPostBySlug(slug)
-
   if (!post) {
     return new Response(null, { status: 404 })
   }
 
   const updatedPost = await incrementLikes(post)
-
   if (!updatedPost) {
     return new Response(null, { status: 404 })
   }
 
   return new Response(JSON.stringify({ likes: updatedPost.Like }), {
     status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
   })
 }
